9717c44845fc129193f9166ac12781bc849d4554
Damian Cichosz <damekjodamek@gmail.com>
2023-10-28 16:43:14 +0200

    Friends Leaderboard

--- Ahoj/app/src/main/java/com/example/ahoj/Leaderboard.java
+++ Ahoj/app/src/main/java/com/example/ahoj/Leaderboard.java
@@ -2,7 +2,10 @@ package com.example.ahoj;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -11,7 +14,10 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -25,6 +31,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -37,8 +45,14 @@ public class Leaderboard extends AppCompatActivity {
 
     private DatabaseReference databaseReference;
     private TextView leaderboardTextView, point, points, points5, leaderboard, downloadingLeaderboard;
-
+    private Button button_friends, button_world;
+    String nick, downloading;
+    SharedPreferences sharedPreferences;
     AdView adview;
+    ArrayList<String> friendsList;
+    ArrayList<UserLeaderboard> friendsLeaderboard;
+    int my_points;
+    LinearLayout linear_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences sharedPreferences2 = PreferenceManager.getDefaultSharedPreferences(this);
@@ -81,6 +95,15 @@ public class Leaderboard extends AppCompatActivity {
         points5 = findViewById(R.id.points5);
         leaderboard = findViewById(R.id.Leaderboard);
         downloadingLeaderboard = findViewById(R.id.downloadingLeaderboard);
+        button_friends = findViewById(R.id.button_friends);
+        button_world = findViewById(R.id.button_world);
+       // leaderboardTextView = findViewById(R.id.leaderboardTextView);
+        linear_layout = findViewById(R.id.linear_layout);
+
+        sharedPreferences = getSharedPreferences("my_app_prefs", Context.MODE_PRIVATE);
+        nick = sharedPreferences.getString("nick", "");
+
+        downloading = downloadingLeaderboard.getText().toString();
 
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
@@ -90,19 +113,42 @@ public class Leaderboard extends AppCompatActivity {
             }
         });
 
+        databaseReference = FirebaseDatabase.getInstance().getReference("Nick/" + nick);
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                my_points = snapshot.child("points").getValue(Integer.class);;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         adview = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adview.loadAd(adRequest);
 
+        button_world.performClick();
+    }
 
-        leaderboardTextView = findViewById(R.id.leaderboardTextView);
+    public void world(View view){
+        linear_layout.removeAllViews();
+
+        button_world.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.almost_transparent));
+        button_friends.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.transparent));
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Nick");
+
+        downloadingLeaderboard.setText(downloading);
 
         Query query = databaseReference.orderByChild("points").startAt(1).limitToLast(10);
 
         query.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 List<UserLeaderboard> userList = new ArrayList<>();
@@ -114,9 +160,7 @@ public class Leaderboard extends AppCompatActivity {
                     UserLeaderboard user = new UserLeaderboard(nick, points);
                     userList.add(user);
                 }
-                downloadingLeaderboard.setText(leaderboard.getText().toString());
 
-                // Sortowanie użytkowników według liczby punktów
                 Collections.sort(userList, new Comparator<UserLeaderboard>() {
                     @Override
                     public int compare(UserLeaderboard user1, UserLeaderboard user2) {
@@ -124,9 +168,12 @@ public class Leaderboard extends AppCompatActivity {
                     }
                 });
 
-                // Tworzenie tekstu do wyświetlenia w TextView
-                StringBuilder leaderboardText = new StringBuilder();
+               // StringBuilder leaderboardText = new StringBuilder();
                 for (int i = 0; i < userList.size(); i++) {
+                    TextView row = new TextView(getApplicationContext());
+                    row.setTextSize(24);
+                    row.setTextColor(R.color.light_grey);
+
                     UserLeaderboard user = userList.get(i);
                     String leaderboardEntry = "";
                     if(user.getPoints() == 1){
@@ -138,11 +185,15 @@ public class Leaderboard extends AppCompatActivity {
                     else if(user.getPoints() > 4){
                         leaderboardEntry += (i + 1) + ": " + user.getNick() + " - " + user.getPoints() + " " + points5.getText().toString() + "\n\n";
                     }
-                    leaderboardText.append(leaderboardEntry);
+                  //  leaderboardText.append(leaderboardEntry);
+                    row.setText(leaderboardEntry);
+                    downloadingLeaderboard.setText(leaderboard.getText().toString());
+                    linear_layout.addView(row);
                 }
 
-                leaderboardTextView.setTextSize(24);
-                leaderboardTextView.setText(leaderboardText.toString());
+
+               // leaderboardTextView.setTextSize(24);
+               // leaderboardTextView.setText(leaderboardText.toString());
             }
 
             @Override
@@ -150,12 +201,111 @@ public class Leaderboard extends AppCompatActivity {
                 leaderboardTextView.setText("Error " + databaseError.getMessage());
             }
         });
+    }
 
 
+    public void friends(View view) {
+        linear_layout.removeAllViews();
 
+        button_world.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.transparent));
+        button_friends.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.almost_transparent));
+
+        friendsList = new ArrayList<>();
+        friendsLeaderboard = new ArrayList<>();
+
+        downloadingLeaderboard.setText(downloading);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Nick/" + nick + "/Friends");
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                        friendsList.add(childSnapshot.getValue(String.class));
+                    }
+
+                    // Po dodaniu wszystkich znajomych do friendsList, możesz kontynuować operacje
+                    getFriendsPoints();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Obsługa błędów
+            }
+        });
+    }
+
+    private void getFriendsPoints() {
+        if (friendsList.isEmpty()) {
+            // Brak znajomych do przetworzenia, można od razu wyświetlić wynik
+            showFriendsLeaderboard();
+        } else {
+            for (String friend : friendsList) {
+                DatabaseReference pointsReference = FirebaseDatabase.getInstance().getReference("Nick/" + friend + "/points");
+                pointsReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        friendsLeaderboard.add(new UserLeaderboard(friend, snapshot.getValue(Integer.class)));
+
+                        // Sprawdź, czy już pobrano punkty wszystkich znajomych
+                        if (friendsLeaderboard.size() == friendsList.size()) {
+                            // Wywołaj sortowanie i wyświetlanie po zakończeniu pobierania wszystkich danych
+                            showFriendsLeaderboard();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        // Obsługa błędów
+                    }
+                });
+            }
+        }
+    }
+
+
+    @SuppressLint("ResourceAsColor")
+    private void showFriendsLeaderboard(){
+        friendsLeaderboard.add(new UserLeaderboard(nick, my_points));
+        Comparator<UserLeaderboard> comparator = new Comparator<UserLeaderboard>() {
+            @Override
+            public int compare(UserLeaderboard user1, UserLeaderboard user2) {
+                return Integer.compare(user2.getPoints(), user1.getPoints()); // Od najwyższego do najniższego
+            }
+        };
+
+        Collections.sort(friendsLeaderboard, comparator);
+
+        for (int i = 0; i < friendsLeaderboard.size(); i++) {
+            TextView row = new TextView(getApplicationContext());
+            row.setTextSize(24);
+
+            String leaderboardEntry = "";
+            if(friendsLeaderboard.get(i).getPoints() == 1){
+                leaderboardEntry = (i + 1) + ": " + friendsLeaderboard.get(i).getNick() + " - " + friendsLeaderboard.get(i).getPoints() + " "  + point.getText().toString() + "\n\n";
+            }
+            else if(friendsLeaderboard.get(i).getPoints() > 1 && friendsLeaderboard.get(i).getPoints() < 5){
+                leaderboardEntry = (i + 1) + ": " + friendsLeaderboard.get(i).getNick() + " - " + friendsLeaderboard.get(i).getPoints() + " " + points.getText().toString() + "\n\n" ;
+            }
+            else if(friendsLeaderboard.get(i).getPoints() > 4){
+                leaderboardEntry += (i + 1) + ": " + friendsLeaderboard.get(i).getNick() + " - " + friendsLeaderboard.get(i).getPoints() + " " + points5.getText().toString() + "\n\n";
+            }
+
+            int textColor = ContextCompat.getColor(getApplicationContext(), R.color.light_grey);
+            if (friendsLeaderboard.get(i).getNick().equals(nick)) {
+                textColor = ContextCompat.getColor(getApplicationContext(), R.color.green);
+            }
+            row.setTextColor(textColor);
+            row.setText(leaderboardEntry);
+            downloadingLeaderboard.setText(leaderboard.getText().toString());
+            linear_layout.addView(row);
+        }
 
 
     }
+
 
     private static class UserLeaderboard {
         private String nick;
